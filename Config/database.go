15cd39e14c9125cdb695762d38d86db71e9ebe99
Bagas-AP <dcool8754@gmail.com>
2023-02-28 12:47:42 +0700

    Init

---
+++ Config/database.go
@@ -0,0 +1,26 @@
+package Config
+
+import (
+	"bcc/Model"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func MakeConnectionDatabase(data *Database) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("user=%s "+
+		"password=%s "+
+		"host=%s "+
+		"TimeZone=Asia/Singapore "+
+		"port=%s "+
+		"dbname=%s", data.SupabaseUser, data.SupabasePassword, data.SupabaseHost, data.SupabasePort, data.SupabaseDbName)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	if err := db.AutoMigrate(&Model.User{}); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
